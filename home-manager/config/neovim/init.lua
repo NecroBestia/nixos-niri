0eab7f1a23b48d8d490be785053d42e7697118d2
NecroBestia <necrobestiaa@gmail.com>
2026-05-22 17:04:06 -0400

    cambios a nvim.nix para estar en la rama inestable

---
+++ home-manager/config/neovim/init.lua
@@ -0,0 +1,14 @@
+-- Opciones generales
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.expandtab = true
+vim.opt.shiftwidth = 2
+vim.opt.tabstop = 2
+vim.opt.smartindent = true
+vim.opt.termguicolors = true
+vim.opt.clipboard = "unnamedplus"
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+
+-- Tecla líder
+vim.g.mapleader = " "
