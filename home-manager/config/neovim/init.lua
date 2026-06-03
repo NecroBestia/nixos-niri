652e1195f607b84f0c831319aa1fd52147fecbf4
NecroBestia <necrobestiaa@gmail.com>
2026-06-02 21:14:15 -0400

    cambios a nvim para usar nvim mini

--- home-manager/config/neovim/init.lua
+++ home-manager/config/neovim/init.lua
@@ -4,11 +4,16 @@ vim.g.maplocalleader = " "
 
 require("vim._core.ui2").enable({})
 
+-- 1. Configuraciones base que no necesitan internet
 require("core.options")
 require("core.keymaps")
+
+-- 2. EL GESTOR DE PAQUETES (El motor arranca aquí)
+require("pack")
+
+-- 3. Ahora que los paquetes existen, cargamos los módulos que dependen de ellos
 require("core.treesitter")
 require("core.lsp")
 
-require("pack")   
-
+-- 4. El tema visual
 vim.cmd.colorscheme("tokyonight")
