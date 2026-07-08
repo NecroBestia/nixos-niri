890efe99bc0620d67aa3896b00e5331ffd5933dc
necrobestia <necrobestiaa@gmail.com>
2026-07-08 18:35:57 -0400

    cambio en documentacion

--- nixos/modules/firewall.nix
+++ nixos/modules/firewall.nix
@@ -29,25 +29,31 @@
 # Útil en portátiles que cambian de red frecuentemente.
 #===================================================================
 { config, pkgs, pkgs-unstable, lib, ... }: {
-  networking.nftables.enable = true;
+  networking.nftables.enable = true; 
+  #DNS mas segura y privada. 
   networking.nameservers = [ "9.9.9.9" ];
-
+  #Configuracion Firewall
   networking.firewall = {
-    enable = true;
+    enable = true; 
     allowPing = true;
     allowedTCPPorts = [22];
-    trustedInterfaces = [ "virbr0" ];
+    trustedInterfaces = ["virbr0"];
   };
-
+  boot.kerne
   boot.kernel.sysctl = {
     "net.ipv4.conf.all.rp_filter" = 1;
     "net.ipv4.conf.default.rp_filter" = 1;
+    # Ignorar pings de broadcast (Evita ataques Smurf y ruido en la red)
     "net.ipv4.icmp_echo_ignore_broadcasts" = 1;
+    # Protección contra mensajes de error ICMP falsos
     "net.ipv4.icmp_ignore_bogus_error_responses" = 1;
+    # Protección contra ataques SYN Flood (DDoS básico)
     "net.ipv4.tcp_syncookies" = 1;
+    # Habilitar el reenvío de paquetes (necesario para VMs)
     "net.ipv4.ip_forward" = 1;
   };
 
+  #opensnitch; permite gestionar conexiones de programas al internet y viceversa. 
   services.opensnitch = {
     enable = lib.mkDefault true;
     package = pkgs-unstable.opensnitch;
@@ -57,5 +63,5 @@
     };
   };
 
-  environment.systemPackages = lib.mkIf config.services.opensnitch.enable (with pkgs-unstable; [ opensnitch-ui ]);
+  environment.systemPackages = lib.mkIf config.services.opensnitch.enable (with pkgs-unstable; [opensnitch-ui]);
 }
